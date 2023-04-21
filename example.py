aa3ff744171ab33ef8e04d8cd015920f5f10b4a8
Ahmad Siavashi <siavashi@aut.ac.ir>
2023-04-21 05:47:49 +0430

    Remove global variables
    The changes introduced in this commit further encapsulate the state of simulation components by the removal of global variables. It is now possible to run multiple simulations simultaneously as simulation instances do not have shared state.

--- example.py
+++ example.py
@@ -1,24 +1,24 @@
-import simulation
-
 # Creating an application called 'nginx' with 2 threads each 2 cycles length.
 from module import App, Vm, Request, User, Pm, DataCenter
 from module.os import OsFcfs
 from module.placement import PlacementFirstFit
 from module.vmm import VmmSpaceShared
+from simulation import Simulation
 
+# Creating an application object called 'nginx' with 3 threads, each 4 cycles length.
 app = App('nginx', (4, 4, 4))
 
-# Creating a virtual machine called 'web server' with 2 cores and 1024 MB of RAM, using the OsFcfs operating system,
+# Creating a virtual machine called 'webserver' with 2 cores and 1 GB of RAM, using the OsFcfs operating system,
 # and scheduling the application 'nginx' on it.
 vm = Vm('webserver', 2, 1024, OsFcfs)
 vm.OS.schedule([app])
 
 # Creating a request for a VM and a user that will request that VM.
-# The request arrives at clock 0 and the user represents the web portal of the cloud provider.
+# The request arrives at clock 0 and the user represents the portal of the cloud provider.
 req = Request(0, vm)
 user = User('portal', [req])
 
-# Creating a physical machine with the name 'hpe gen10', with 2 cores (each 2 cycles per simulation time unit, i.e. tick)
+# Creating a physical machine with the name 'hpe gen10', with 2 cores (each 2 cycles per simulation time unit)
 # and 2 GB of RAM, and using the VmmSpaceShared virtual machine manager.
 pm = Pm('hpe gen10', (2, 2), 2048, VmmSpaceShared)
 
@@ -27,5 +27,4 @@ pm = Pm('hpe gen10', (2, 2), 2048, VmmSpaceShared)
 datacenter = DataCenter('boston region', [pm], PlacementFirstFit)
 
 # Initializing the simulation with the user and data center and then starting the simulation.
-simulation.init(user, datacenter)
-simulation.start()
+Simulation(user, datacenter).start()
