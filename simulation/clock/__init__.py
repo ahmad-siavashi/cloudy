ff92ccee6f1131ffde14b85283f713c97795ac96
Ahmad Siavashi <siavashi@aut.ac.ir>
2023-04-25 04:54:27 +0430

    Convert static Clock class to non-static

--- simulation/clock/__init__.py
+++ simulation/clock/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import ClassVar
-
 
 class Clock:
     """
@@ -10,22 +8,20 @@ class Clock:
 
     Attributes
     ==========
-    - _tick (ClassVar[int], private): a private static class variable to keep the current time
+    - _tick (int, private): a private class variable to keep the current time
     """
-    _tick: ClassVar[int] = 0
+    _tick: int = 0
 
-    @staticmethod
-    def reset() -> int:
+    def reset(self) -> int:
         """
         The reset function resets the clock to 0.
 
         :return: The new current time
         """
-        Clock._tick = 0
-        return Clock.now()
+        self._tick = 0
+        return self.now()
 
-    @staticmethod
-    def increment(ticks: int = 1) -> int:
+    def increment(self, ticks: int = 1) -> int:
         """
         The increment function is used to increment the clock by a given number of ticks where each tick represents one simulation time unit.
         The default value for ticks is 1, so if no argument is passed in, the clock will be incremented by 1 tick.
@@ -33,16 +29,13 @@ class Clock:
         :param ticks: int: Increment the clock by a certain number of ticks
         :return: The new current time
         """
-        Clock._tick += ticks
-        return Clock.now()
+        self._tick += ticks
+        return self.now()
 
-    @staticmethod
-    def now() -> int:
+    def now(self) -> int:
         """
-        The now function is a static method of the Clock class.
-        It returns the current time, which is stored in a private variable called _tick.
-        The now function can be used to get the current time without having to create an instance of Clock.
+        The now function returns the current time, which is stored in a private variable called _tick.
 
-        :return: The value of the _tick attribute in the clock class
+        :return: The value of the _tick attribute
         """
-        return Clock._tick
+        return self._tick
